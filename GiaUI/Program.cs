8df5ee3051466a2317a2f79ebc1911330130837a
YZukio <100427103+Ykizakyi-Zukio@users.noreply.github.com>
2026-05-20 16:09:53 +0600

    v

--- GiaUI/Program.cs
+++ GiaUI/Program.cs
@@ -1,10 +1,8 @@
 ﻿using GiaUI.Data;
 using GiaUI.Lib;
 using GiaUI.Marshal;
-using System;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
@@ -14,10 +12,31 @@ class Program
         Console.CursorVisible = false;
         ConsoleHelper.EnableAnsi();
 
-        Rainbow rnbw = new() { Text = "Hello world!"};
+        Rainbow rnbw = new(0.5f, 2) { Text = "Hello world!"};
         Shade shade = new( new(140, 30, 67), 0.6f) { Text = "Hello world, again.." };
 
-        Animation animation = new(20, [rnbw, shade]);
-        animation.Start();
+        Animation animation = new(20, [rnbw]);
+        Console.Write(rnbw.Decorate("Hello worlddddddd") + "HELLO");
+        //animation.Start();
+
+        StringBuilder Buffer = new();
+        float sinAngle = 0.3f;
+        byte upper = 2;
+        double Phase = 0;
+        string text = "Hello world";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            int r = (int)(Math.Sin(sinAngle * i + Phase + 0) * 127 + 128);
+            int g = (int)(Math.Sin(sinAngle * i + Phase + upper) * 127 + 128);
+            int b = (int)(Math.Sin(sinAngle * i + Phase + upper * 2) * 127 + 128);
+
+            Buffer.Append($"\x1b[38;2;{r};{g};{b}m{text[i]}");
+        }
+
+        Buffer.Append("\x1b[0m");
+        Console.WriteLine(Buffer.ToString());
+
+        Console.ReadKey();
     }
 }
